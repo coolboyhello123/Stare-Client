79bf94ba991394d7f8e1e6259e4834c476818931
coolboyhello123 <domsterjohn@gmail.com>
2026-03-16 16:09:23 -0400

    Update Velocity.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Velocity.java
@@ -8,7 +8,7 @@ import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 
 public class Velocity extends Module {
     public Velocity() {
-        super("Velocity", "Removes velocity from explosions and entities", Category.PLAYER, true, false, false);
+        super("Anti Knockback", "Removes the knockback from explosions and entities", Category.PLAYER, true, false, false);
     }
 
     @Subscribe private void onPacketReceive(PacketEvent.Receive event) {
